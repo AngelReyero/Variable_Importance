ee7498e5882bf2f527a668e84450a5145f3739ef
AngelReyero <angelreyerolobo@gmail.com>
2024-06-05 17:02:59 +0200

    CPIvsPFI convergence rates until 15000

--- PFIvsCPI.py
+++ PFIvsCPI.py
@@ -11,6 +11,7 @@ from joblib import Parallel, delayed
 from scipy.linalg import cholesky
 from scipy.stats import norm
 from sklearn.preprocessing import OneHotEncoder
+#from .utils import compute_loco
 seed=2024
 
 
@@ -22,11 +23,10 @@ seed=2024
 
 snr=4
 p=3
-n=10000
+n=15000
 intra_cor=0.8
 cor_mat=np.zeros((p,p))
-cor_mat[0:(p-1),0]=intra_cor
-cor_mat[0,0:(p-1)]=intra_cor
+cor_mat[0:p,0:p]=intra_cor
 np.fill_diagonal(cor_mat, 1)
 x = norm.rvs(size=(p, n), random_state=seed)
 c = cholesky(cor_mat, lower=True)
@@ -56,7 +56,7 @@ data_enc.cov()
 
 
 # %%
-inter=[100,200, 350, 500, 750, 1000, 2000, 3500, 5000, 7500, 10000]
+inter=[100,200, 350, 500, 750, 1000, 2000, 3500, 5000, 7500,9000, 11000, 13000, 15000]
 imp=np.zeros((2, len(inter), 3))
 pval=np.zeros((2, len(inter), 3))
 for (i,n) in enumerate(inter): 
@@ -106,14 +106,15 @@ fig.suptitle("Convergence rates of CPI vs PFI",fontsize=16)
 for i in range(p):
     axs[0, i].plot(inter, imp[0,:,i], label="CPI", linestyle="dashed")
     axs[0, i].plot(inter, imp[1,:,i], label="PFI")
-    axs[0, i].set_title(r'Importance $\beta$'+str(i), fontsize=14)
+    axs[0, i].set_title(r'Importance $x$'+str(i), fontsize=14)
     axs[1, i].plot(inter, -np.log10(pval[0,:,i]+1e-10), label="CPI", linestyle="dashed")
     axs[1, i].plot(inter, -np.log10(pval[1,:,i]+1e-10), label="PFI")
-    axs[1,i].axhline(y=-np.log10(0.05), color='r', linestyle='-')
-    axs[1, i].set_title(r'-log10(p_value) $\beta$'+str(i), fontsize=14)
+    axs[1,i].axhline(y=-np.log10(0.05), color='r', linestyle='-', label=r"$-\mathrm{log}_{10}(\alpha)$")
+    axs[1, i].set_title(r'-log10(p_value) $x$'+str(i), fontsize=14)
     axs[0,i].legend()
     axs[1,i].legend()
 
+fig.text(0.53, 0, r'$n$', ha='center', va='center')
 plt.tight_layout(rect=[0, 0, 1, 0.95]) 
 fig.savefig("CPIvsPFI_conv-rates.pdf")
 
@@ -140,8 +141,7 @@ for (i,cor) in enumerate(intra_cor):
     print("With correlation="+str(cor))
     #First we construct the sample with the third useless covariate with correlation=cor
     cor_mat=np.zeros((p,p))
-    cor_mat[0:(p-1),0]=cor
-    cor_mat[0,0:(p-1)]=cor
+    cor_mat[0:p,0:p]=cor
     np.fill_diagonal(cor_mat, 1)
 
     c = cholesky(cor_mat, lower=True)
@@ -208,13 +208,15 @@ fig.suptitle("Correlation effects on CPI vs PFI",fontsize=16)
 for i in range(p):
     axs[0, i].plot(intra_cor, imp2[0,:,i], label="CPI", linestyle="dashed")
     axs[0, i].plot(intra_cor, imp2[1,:,i], label="PFI")
-    axs[0, i].set_title(r'Importance $\beta$'+str(i), fontsize=14)
+    axs[0, i].set_title(r'Importance $x$'+str(i), fontsize=14)
     axs[1, i].plot(intra_cor, -np.log10(pval2[0,:,i]+1e-10), label="CPI", linestyle="dashed")
     axs[1, i].plot(intra_cor, -np.log10(pval2[1,:,i]+1e-10), label="PFI")
-    axs[1,i].axhline(y=-np.log10(0.05), color='r', linestyle='-')
-    axs[1, i].set_title(r'-log10(p_value) $\beta$'+str(i), fontsize=14)
+    axs[1,i].axhline(y=-np.log10(0.05), color='r', linestyle='-',  label=r"$-\mathrm{log}_{10}(\alpha)$")
+    axs[1, i].set_title(r'-log10(p_value) $x$'+str(i), fontsize=14)
     axs[0,i].legend()
     axs[1,i].legend()
+
+fig.text(0.53, 0, r'$\rho$', ha='center', va='center')
 
 plt.tight_layout(rect=[0, 0, 1, 0.95]) 
 fig.savefig("CPIvsPFI_corr.pdf")
@@ -229,7 +231,7 @@ snr=4
 p=3
 n=1000
 x = norm.rvs(size=(p, n), random_state=seed)
-noise_arr=[0, 0.1, 0.3, 0.5, 0.75, 1, 1.5, 2,3.5, 5, 10]
+noise_arr=[0, 0.05, 0.1,0.2, 0.35, 0.5, 0.75, 1, 1.5, 2,3.5, 5]
 
 imp3=np.zeros((2, len(noise_arr), 3))
 pval3=np.zeros((2, len(noise_arr), 3))
@@ -304,11 +306,11 @@ fig.suptitle("Conditionally independent covariate with CPI vs PFI",fontsize=16)
 for i in range(p):
     axs[0, i].plot(noise_arr, imp3[0,:,i], label="CPI", linestyle="dashed")
     axs[0, i].plot(noise_arr, imp3[1,:,i], label="PFI")
-    axs[0, i].set_title(r'Importance $\beta$'+str(i), fontsize=14)
+    axs[0, i].set_title(r'Importance $x$'+str(i), fontsize=14)
     axs[1, i].plot(noise_arr, -np.log10(pval3[0,:,i]+1e-10), label="CPI", linestyle="dashed")
     axs[1, i].plot(noise_arr, -np.log10(pval3[1,:,i]+1e-10), label="PFI")
     axs[1,i].axhline(y=-np.log10(0.05), color='r', linestyle='-')
-    axs[1, i].set_title(r'-log10(p_value) $\beta$'+str(i), fontsize=14)
+    axs[1, i].set_title(r'-log10(p_value) $x$'+str(i), fontsize=14)
     axs[0,i].legend()
     axs[1,i].legend()
 
@@ -322,17 +324,22 @@ fig.suptitle("Conditionally independent covariate with CPI vs PFI",fontsize=16)
 for i in range(p):
     axs[0, i].plot(noise_arr[0:7], imp3[0,0:7,i], label="CPI", linestyle="dashed")
     axs[0, i].plot(noise_arr[0:7], imp3[1,0:7,i], label="PFI")
-    axs[0, i].set_title(r'Importance $\beta$'+str(i), fontsize=14)
+    axs[0, i].set_title(r'Importance $x$'+str(i), fontsize=14)
     axs[1, i].plot(noise_arr[0:7], -np.log10(pval3[0,0:7,i]+1e-10), label="CPI", linestyle="dashed")
     axs[1, i].plot(noise_arr[0:7], -np.log10(pval3[1,0:7,i]+1e-10), label="PFI")
-    axs[1,i].axhline(y=-np.log10(0.05), color='r', linestyle='-')
-    axs[1, i].set_title(r'-log10(p_value) $\beta$'+str(i), fontsize=14)
+    axs[1,i].axhline(y=-np.log10(0.05), color='r', linestyle='-', label=r"$-\mathrm{log}_{10}(\alpha)$")
+    axs[1, i].set_title(r'-log10(p_value) $x$'+str(i), fontsize=14)
     axs[0,i].legend()
     axs[1,i].legend()
 
+fig.text(0.53, 0, r'$\lambda$', ha='center', va='center')
+#fig.text(0.06, 0.5, 'Common Y Label', ha='center', va='center', rotation='vertical')
 plt.tight_layout(rect=[0, 0, 1, 0.95]) 
 
 fig.savefig("CPIvsPFI_cond-indep_2.pdf")
 
 
 # %%
+
+#LOCO
+
